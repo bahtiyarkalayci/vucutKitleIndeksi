357d8d56ad8ed106d0e98931a6cebcf44b03ef1b
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-10 19:01:28 +0300

    https://www.patika.dev/tr

--- src/Vucut.java
+++ src/Vucut.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Vucut {
+import java.util.Scanner;
+
+public class Vucut {
+    public static void main(String[] args) {
+        double boy,kilo,bodyMassIndex;
+        Scanner input=new Scanner(System.in);
+        System.out.print("boyunuzu metre cinsinden giriniz:");
+        boy= input.nextDouble();
+        System.out.print("kilonuzu giriniz:");
+        kilo=input.nextDouble();
+        bodyMassIndex=kilo/(boy*boy);
+        System.out.print("vücut kitle indeksiniz="+bodyMassIndex);
+    }
 }
